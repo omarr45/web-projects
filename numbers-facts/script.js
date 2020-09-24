bdaa1caaa69847b88a78e25b457704abdd361fb6
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-24 02:34:02 +0200

    numbers facts

---
+++ numbers-facts/script.js
@@ -0,0 +1,28 @@
+const API1 = 'https://numbersapi.p.rapidapi.com/'
+const API2 = '/math?fragment=true&json=true&rapidapi-key=1347e918demshb2956622439bb56p118b7cjsn8820cdaaaffe';
+
+const TEST = 'https://numbersapi.p.rapidapi.com/123/math?fragment=true&json=true&rapidapi-key=1347e918demshb2956622439bb56p118b7cjsn8820cdaaaffe';
+
+const facts = document.getElementById('facts');
+const myNumber = document.getElementById('dispNumber');
+var query = 45;
+
+async function Search() {
+
+    if (document.Form.Number.value != "")
+        query = document.Form.Number.value;
+
+    const response = await fetch(API1 + query + API2);
+    const data = await response.json();
+    console.log(data);
+
+    myNumber.innerHTML = query;
+
+    if (data.found) {
+        facts.innerHTML = data.text;
+
+    } else {
+        facts.innerHTML = "Oops, this looks like a boring number.";
+    }
+
+}
