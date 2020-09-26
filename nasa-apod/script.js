f8790edfe743079423cb22c946613fb45e6326fd
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-09-26 14:06:50 +0200

    handeled errors

--- nasa-apod/script.js
+++ nasa-apod/script.js
@@ -1,19 +1,40 @@
 const API = "https://api.nasa.gov/planetary/apod?api_key=SnpgUIlwedUb1b5JiAWAhTcapNgZ1PaQemPa6N05"
 
-async function getData() {
-    const response = await fetch(API);
-    const data = await response.json();
-    console.log(data);
+let name = document.getElementById("img-title");
+let photo = document.getElementById("apod");
+let author = document.getElementById("author")
+let desc = document.getElementById("desc");
+let click = document.getElementById("clickable");
+let link = document.getElementById("link");
 
-    let name = document.getElementById("img-title");
-    let author = document.getElementById("author")
-    let desc = document.getElementById("desc");
-    let link = document.getElementById("link");
+function showData() {
+    fetch(API)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
 
-    name.innerHTML = data.title;
-    author.innerHTML = "By: " + data.copyright;
-    desc.innerHTML = data.explanation;
-    link.href = data.hdurl;
+            name.innerHTML = data.title;
+            author.innerHTML = "By: " + data.copyright;
+            desc.innerHTML = data.explanation;
+            photo.src = data.url;
+            link.href = data.hdurl;
+
+            if (data.title == "undefined" || data.explanation == "undefined") {
+                photo.src = "error.png";
+                name.innerHTML = "Sorry!<br>Today's image hasn't been uploaded yet";
+                author.style.display = "none";
+                desc.style.display = "none";
+                click.style.display = "none";
+            }
+        })
+        .catch((e) => {
+            console.error('Error:', e);
+            photo.src = "error.png";
+            name.innerHTML = "Sorry!<br>Today's image hasn't been uploaded yet";
+            author.style.display = "none";
+            desc.style.display = "none";
+            click.style.display = "none";
+        })
 }
 
-getData();
+showData();
